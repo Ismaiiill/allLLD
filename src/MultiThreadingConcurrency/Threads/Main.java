6f5b7a2562c59bcef906c7a6b9a09fd78c0b56c8
MD ISMAIL <ismail1332000@gmail.com>
2025-12-20 19:44:44 +0530

    hey

--- src/MultiThreadingConcurrency/Threads/Main.java
+++ src/MultiThreadingConcurrency/Threads/Main.java
@@ -1,22 +1,20 @@
 package src.MultiThreadingConcurrency.Threads;
 
 public class Main {
-    public static void main(String[] args){
-        Object lock;
-        lock=new Object();
-        System.out.println("main is starting ");
-//        Thread1 thread1=new Thread1("Thread1");
-//        thread1.start();
-//        thread1.run();
-        Runnable thread2=new Thread2();
-        Thread thread3=new Thread(new Thread2());
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("starting");
+//        Thread.currentThread().wait(8);
+        Thread1 thread1=new Thread1("thread1");
+        Thread1 thread3=new Thread1("thread3");
+        Thread2 thread2=new Thread2();
+        Thread thread5=new Thread(thread2);
+//        thread1.notifyAll();
+        System.out.println(Thread.currentThread());
+        Thread.sleep(5);
+        thread1.start();
         thread3.run();
-        thread3.start();
-        System.out.println("main is ending ");
-//        t1,t2,t3
-        synchronized (new String("dfdfsf")){
-            System.out.println("lsdfs");
-        }
+        System.out.println("ending");
+
 
     }
 }
