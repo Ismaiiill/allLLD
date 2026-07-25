bcd0ab34ca2ee78b017cce9b9c7d4efae8a02bdb
Md Ismail <ismail1332000@gmail.com>
2026-07-25 15:03:52 +0530

    Pub-sub done

--- src/MultiThreadingConcurrency/practiceInterview/practice2/Main.java
+++ src/MultiThreadingConcurrency/practiceInterview/practice2/Main.java
@@ -3,6 +3,7 @@ package src.MultiThreadingConcurrency.practiceInterview.practice2;
 public class Main {
     public static void main(String[] args) throws InterruptedException{
 
+        /*
         Stack stack=new Stack(5);
         System.out.println(stack.isEmpty());
         System.out.println(stack.isFull());
@@ -29,9 +30,32 @@ public class Main {
         t2.start();
         t1.join();
         t2.join();
+        */
+        BlockingQueue queue=new BlockingQueue(5);
+        Thread t1=new Thread(()->{
+            for (int i=1;i<=1011;i++){
+                try {
+                    System.out.println(queue.add(i));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread t2=new Thread(()->{
+            for (int i=1;i<=1010;i++){
+                try {
+                    System.out.println(queue.remove());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
 
 
     }
-
-
 }
